5da5475bff4c5a1bf72f2aa4006fe2e096a9969b
AOthman97 <44799617+AOthman97@users.noreply.github.com>
2022-08-27 11:17:43 +0200

    Added the AccommondationTypes controller.

---
+++ Etaa.API/Controllers/AccommondationTypesController.cs
@@ -0,0 +1,93 @@
+﻿namespace Etaa.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccommondationTypesController : ControllerBase
+    {
+        // Firstly Inject the IBaseRepo
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AccommondationTypesController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult GetAccommondationTypes()
+        {
+            return Ok(_unitOfWork.AccommodationTypes.GetAll().ToList());
+        }
+
+        [HttpPost("AddSingle")]
+        public IActionResult AddSingle(AccommodationTypeDto typeDto)
+        {
+            try
+            {
+                AccommodationType type = new()
+                {
+                    NameAr = typeDto.NameAr,
+                    NameEn = typeDto.NameEn
+                };
+                var AccommodationType = _unitOfWork.AccommodationTypes.Add(type);
+                int NumberAffected = _unitOfWork.Complete();
+                if (NumberAffected > 0)
+                    return Ok(AccommodationType);
+                else
+                    return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("UpdateSingle")]
+        public IActionResult UpdateSingle(AccommodationTypeDto typeDto)
+        {
+            try
+            {
+                AccommodationType type = new()
+                {
+                    AccommodationTypeId = typeDto.AccommodationTypeId,
+                    NameAr = typeDto.NameAr,
+                    NameEn = typeDto.NameEn
+                };
+                var AccommodationType = _unitOfWork.AccommodationTypes.Update(type);
+                int NumberAffected = _unitOfWork.Complete();
+                if (NumberAffected > 0)
+                    return Ok(AccommodationType);
+                else
+                    return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("DeleteSingle")]
+        public IActionResult DeleteSingle(int AccommodationTypeId)
+        {
+            try
+            {
+                var IsDeleted = _unitOfWork.AccommodationTypes.Delete(AccommodationTypeId);
+                if (IsDeleted)
+                {
+                    int NumberAffected = _unitOfWork.Complete();
+                    if (NumberAffected > 0)
+                        return Ok(true);
+                    else
+                        return BadRequest();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
